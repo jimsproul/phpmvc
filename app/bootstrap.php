0d37202a6321d9bb07146b8a39e6e6340394fcd1
Jim Sproul <jimsproul@users.noreply.github.com>
2021-02-22 15:44:11 -0600

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,10 +4,13 @@
 Bootstrap file has all the included files
 
 */
+
+// require helpers
+
 require_once('config/config.php');
 require_once('helpers/urlHelper.php');
 
-// Class Autoloader
+// Register a class autoloader
 
 spl_autoload_register(function($className) {
 	require_once('libraries/'.$className.'.php');
